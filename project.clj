d380daf8789ff7a3b7ca71a0292a7f3d226ac109
Earl St Sauver <estsauver@gmail.com>
2014-09-17 15:07:51 -0500

    Update project license url

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
+
 ;  Copyright 2014 The Climate Corporation
-;
+
 ;  Licensed under the Apache License, Version 2.0 (the "License");
 ;  you may not use this file except in compliance with the License.
 ;  You may obtain a copy of the License at
-;
+
 ;      http://www.apache.org/licenses/LICENSE-2.0
-;
+
 ;  Unless required by applicable law or agreed to in writing, software
 ;  distributed under the License is distributed on an "AS IS" BASIS,
 ;  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +17,7 @@
   :description "A Geojson validator using prismatic schema"
   :url "https://github.com/TheClimateCorporation/geojson-schema"
   :license {:name "Apache 2.0"
-            :url "http://www.opensource.org/licenses/mit-license.php"}
+            :url "http://opensource.org/licenses/Apache-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [prismatic/schema "0.2.6"]]
   :profiles {:dev {:dependencies [[cheshire "5.3.1"]]}})
