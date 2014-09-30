031afa6e4ed2e4a27eb70da3cd7f4dead150dc62
Earl St Sauver <estsauver@climate.com>
2014-09-30 10:35:14 -0700

    Bump prismatic schema version to 0.3.0

--- project.clj
+++ project.clj
@@ -18,6 +18,6 @@
   :license {:name "Apache 2.0 License"
             :url "http://opensource.org/licenses/Apache-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [prismatic/schema "0.2.6"]]
+                 [prismatic/schema "0.3.0"]]
   :profiles {:test {:dependencies [[cheshire "5.3.1"]]
                     :resource-paths ["resources" "test-resources"]}})
